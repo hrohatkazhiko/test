f992ce19af47f80666b0fe995fe6d0df6a8bb302
hrohatkazhiko <132198586+hrohatkazhiko@users.noreply.github.com>
2023-04-30 19:12:24 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>テストページ</title>
+</head>
+<body>
+    <h1>Pテストページ</h1>
+    <?php
+        echo "<p>PHPバージョン: " . phpversion() . "</p>";
+    ?>
+</body>
+</html>
